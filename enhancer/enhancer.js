ac9a75d8481bbc9b825665ab079a4a2494813c3b
Moonkaman <nickbatmanglidj@gmail.com>
2019-03-04 15:22:00 -0700

    Create test for repair success functions and tests

--- enhancer/enhancer.js
+++ enhancer/enhancer.js
@@ -5,7 +5,19 @@ module.exports = {
 }
 
 function success(item) {
+  if(item.enhancement <= 14 && item.durability > 25) {
+    item.enhancement++;
+    item.displayName = `[${generateName(item.enhancement)}] ${item.name}`;
+    return item;
+  }
 
+  if(item.enhancement >= 15 && item.durability > 10) {
+    item.enhancement++;
+    item.displayName = `[${generateName(item.enhancement)}] ${item.name}`;
+    return item;
+  }
+  
+  return 'Item durablility too low.';
 }
 
 function fail(item) {
@@ -13,5 +25,30 @@ function fail(item) {
 }
 
 function repair(item) {
+  return {...item, durability: 100};
+}
 
+function generateName(lvl) {
+  if(lvl === 0) {
+    return ''
+  } else if(lvl > 0 && lvl < 16) {
+    return `+${lvl}`
+  } else {
+    switch(lvl) {
+      case 16:
+      return 'PRI'
+
+      case 17:
+      return 'DUO'
+
+      case 18:
+      return 'TRI'
+
+      case 19:
+      return 'TET'
+       
+      case 20:
+      return 'PEN'
+    }
+  }
 }
--- enhancer/enhancer.spec.js
+++ enhancer/enhancer.spec.js
@@ -1,1 +1,21 @@
 const { success, fail, repair } = require('./enhancer.js');
+
+
+describe('repair()', () => {
+  it('sets durability back to 100', () => {
+    const item = {name: 'Sword', type: 'weapon', durability: 80, enhancement: 5};
+    expect(repair(item)).toEqual({name: 'Sword', type: 'weapon', durability: 100, enhancement: 5});
+  });
+});
+
+describe('success()', () => {
+  it('increments enhancement and sets name', () => {
+    const item = {name: 'Sword', type: 'weapon', durability: 80, enhancement: 5};
+    const item2 = {name: 'Sword', type: 'weapon', durability: 20, enhancement: 14};
+    const item3 = {name: 'Sword', type: 'weapon', durability: 80, enhancement: 15};
+
+    expect(success(item)).toEqual({displayName: '[+6] Sword', type: 'weapon', name: 'Sword', durability: 80, enhancement: 6});
+    expect(success(item2)).toEqual('Item durablility too low.')
+    expect(success(item3)).toEqual({displayName: '[PRI] Sword', type: 'weapon', name: 'Sword', durability: 80, enhancement: 16});
+  })
+})
